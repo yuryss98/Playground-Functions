bca8d58dafdbe8cd7cb3c4ca09e409806af94409
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-05-18 18:22:37 -0300

    Requisito 7 completo

--- src/challenges.js
+++ src/challenges.js
@@ -46,8 +46,13 @@ function highestCount(arrayDeFora) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    return 'cat1';
+  } else if (Math.abs(cat2 - mouse) < Math.abs(cat1 - mouse)) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
